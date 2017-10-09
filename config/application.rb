512dc2c39cf026b0c5abef0df7a41a0a04e5d131
shenlinxue <842393426@qq.com>
2017-10-09 21:00:46 +0800

    init project

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Mytest
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |generator|
+		   generator.asset false
+		   generator.test_frameword false
+		   generator.skip_routes true
+		end
+  end
+end
